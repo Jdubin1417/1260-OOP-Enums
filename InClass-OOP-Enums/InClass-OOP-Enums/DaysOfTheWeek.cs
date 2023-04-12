ead416b391e2d2c84c705b8f5ac614d158099db5
Justin Nicholas Dubin <99613905+Jdubin1417@users.noreply.github.com>
2023-04-12 13:04:26 -0400

    Add files via upload

---
+++ InClass-OOP-Enums/InClass-OOP-Enums/DaysOfTheWeek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_OOP_Enums
+{
+    public enum DaysOfTheWeek
+    {
+        Sunday,     //0
+        Monday,     //1
+        Tuesday,    //2
+        Wednesday, 
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ InClass-OOP-Enums/InClass-OOP-Enums/Driver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_OOP_Enums
+{
+    public class Driver
+    {
+        public static void Main()
+        {
+            //Console.WriteLine(Divide(10, 3));
+            //Console.WriteLine(Divide(10.0, 3.0));
+
+            string name = "bob";
+
+            DaysOfTheWeek monday = DaysOfTheWeek.Sunday;
+            string mondayStr = monday.ToString(); //convert from enum -> string
+
+            Console.WriteLine(monday);
+            Console.WriteLine(mondayStr);
+
+            Console.WriteLine("Please enter a day of the week:");
+            string userInput = Console.ReadLine();
+            DaysOfTheWeek userDay = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userInput); //convert from string->enum
+
+            switch(userDay)
+            {
+                case DaysOfTheWeek.Sunday:
+                case DaysOfTheWeek.Saturday:
+                    Console.WriteLine("It's the weekend");
+                    break;
+                case DaysOfTheWeek.Monday:
+                case DaysOfTheWeek.Tuesday:
+                case DaysOfTheWeek.Wednesday:
+                case DaysOfTheWeek.Thursday:
+                    Console.WriteLine("It's the middle of the week");
+                    break;
+                case DaysOfTheWeek.Friday:
+                    Console.WriteLine("It's almost the weekend");
+                    break;
+                default:
+                    break;
+            }
+
+            int myNum = (int)DaysOfTheWeek.Wednesday;
+            Console.WriteLine(myNum);
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        /*
+        public static int Divide(int b, int a)
+        {
+            return a / b;
+        }
+        */
+
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        /*
+        public static int Divide(double a, double b)
+        {
+            return (int)(a / b);
+        }
+        */
+    }
+}
